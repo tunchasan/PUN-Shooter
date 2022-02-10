4b7338031b1f9cd1014296a566afd528133c6b9b
Hasan-Tunc <39636292+tunchasan@users.noreply.github.com>
2022-02-10 23:18:33 +0300

    Feat : Creating Game Scenes

---
+++ Assets/_Main/Scripts/Game/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+namespace Com.MyCompany.MyGame
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        #region Photon Callbacks
+
+        /// <summary>
+        /// Called when the local player left the room. We need to load the launcher scene.
+        /// </summary>
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+    }
+}
