ed11e43a466b907b5f4b97126bfd2a65d31367ae
Hasan-Tunc <39636292+tunchasan@users.noreply.github.com>
2022-02-12 13:05:23 +0300

    Feat : Creating Player Prefab & Handling Animation, Movement, Camera

---
+++ Assets/_Main/Scripts/Game/Player/PlayerAnimatorManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+namespace Com.MyCompany.MyGame
+{
+    public class PlayerAnimatorManager : MonoBehaviour
+    {
+        #region Private Serializable Fields
+
+        [SerializeField]
+        private float directionDampTime = 0.25f;
+
+        #endregion
+        
+        #region Private Fields
+
+        private Animator _animator = null;
+
+        #endregion
+        
+        #region MonoBehaviour Callbacks
+
+        // Use this for initialization
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            
+            if (!_animator)
+                Debug.LogError("PlayerAnimatorManager is Missing Animator Component", this);
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (!_animator)
+                return;
+            
+            // deal with Jumping
+            var stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+            // only allow jumping if we are running.
+            if (stateInfo.IsName("Base Layer.Run"))
+            {
+                // When using trigger parameter
+                if (Input.GetButtonDown("Fire2"))
+                    _animator.SetTrigger("Jump");
+            }
+
+            var h = Input.GetAxis("Horizontal");
+            var v = Input.GetAxis("Vertical");
+            
+            v = v < 0 ? 0 : v;
+
+            _animator.SetFloat("Speed", h * h + v * v);
+            
+            _animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+        }
+
+        #endregion
+    }
+}
